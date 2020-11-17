dbef4e0358aa80972778b5c701999cdd76fc150e
LINDA HINTZE <lindahintze@hotmail.com>
2020-11-17 16:54:30 +0100

    Last commit for today.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { quiz } from 'reducers/quiz'
 
 export const Options = () => {
+  const [disabled, setDisabled] = useState(false)
   const dispatch = useDispatch()
+
   const question = useSelector(
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
   )
   
   if (!question) {
     return <h1>Oh no! I could not find the current answers!</h1>
-
   }
-  console.log(question)
 
   return (
     <form>
@@ -20,7 +20,8 @@ export const Options = () => {
         return (
           <div>
             <label>{option}</label>
-            <input type="radio" name="option" onChange={() => dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))}/>
+            <input type="radio" name="option" onChange={( ) => dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))}/>
+            {/* ? index !== question.correctAnswerIndex : setDisable  */}
           </div>
         )
       })}
